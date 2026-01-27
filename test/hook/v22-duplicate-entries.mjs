fdc0b3d5863a1338586e25a94b831fee1bd8bd0b
Trent Mick <trentm@gmail.com>
2026-01-27 04:05:57 -0800

    fix: fix a couple issues with duplicate entries and specifier (submodule) matching (#237)

---
+++ test/hook/v22-duplicate-entries.mjs
@@ -0,0 +1,17 @@
+import { deepStrictEqual } from 'assert'
+import { Hook } from '../../index.js'
+
+const hits = []
+Hook(['some-external-module', 'some-external-module'], { internals: true }, (exports, name, baseDir) => {
+  hits.push(name)
+})
+
+await import('../fixtures/load-external-modules.mjs')
+
+// Should get hits for each of the two 'some-external-module' Hook entries.
+deepStrictEqual(hits, [
+  'some-external-module/sub.mjs',
+  'some-external-module/sub.mjs',
+  'some-external-module/index.mjs',
+  'some-external-module/index.mjs'
+])
---
+++ test/hook/v22-hook-module-and-specifier.mjs
@@ -0,0 +1,15 @@
+import { deepStrictEqual } from 'assert'
+import { Hook } from '../../index.js'
+
+const hits = []
+Hook(['some-external-module', 'some-external-module/sub'], { internals: true }, (exports, name, baseDir) => {
+  hits.push(name)
+})
+
+await import('../fixtures/load-external-modules.mjs')
+
+deepStrictEqual(hits, [
+  'some-external-module/sub.mjs', // module name match, internals:true
+  'some-external-module/sub', // specifier match
+  'some-external-module/index.mjs' // module name match, internals:true
+])
